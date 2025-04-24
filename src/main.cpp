910c1ff8b93eeb622b6a28dee2cf5086e05bfacf
Ethan Castillo <eduardo_ethandrake@yahoo.com.mx>
2025-04-23 19:05:54 -0600

    initial subs commit

--- src/main.cpp
+++ src/main.cpp
@@ -1,9 +1,9 @@
 /**
- * @file      ModemMqttPulishExample.ino
+ * @file      ModemMqttSubscribeExample.ino
  * @author    Lewis He (lewishe@outlook.com)
  * @license   MIT
  * @copyright Copyright (c) 2022  Shenzhen Xin Yuan Electronic Technology Co., Ltd
- * @date      2022-09-16
+ * @date      2022-09-28
  *
  */
 #include <Arduino.h>
@@ -11,33 +11,31 @@
 #include "XPowersLib.h"
 #include "utilities.h"
 
-
-//Pitches for buzzer implementation
-#include "pitches.h" 
-#define BUZZER_PIN 18
-int melody[] = {
-  NOTE_C4, NOTE_G3, NOTE_G3, NOTE_A3, NOTE_G3, 0, NOTE_B3, NOTE_C4
-};
-
 XPowersPMU  PMU;
 
 // See all AT commands, if wanted
+//#define DUMP_AT_COMMANDS
 
 #define TINY_GSM_RX_BUFFER 1024
 
 #define TINY_GSM_MODEM_SIM7080
 #include <TinyGsmClient.h>
-#include "utilities.h"
 
 
+//#ifdef DUMP_AT_COMMANDS
+//#include <StreamDebugger.h>
+//StreamDebugger debugger(Serial1, Serial);
+//TinyGsm        modem(debugger);
+//#else
 #define SerialAT  Serial1
 TinyGsm        modem(SerialAT);
+//#endif
 
 const char *register_info[] = {
-    "Not registered, MT is not currently searching an operator to register to.The GPRS service is disabled, the UE is allowed to attach for GPRS if requested by the user.",
+    "Not registered, MT is not currently searching an operator to register to. The GPRS service is disabled, the UE is allowed to attach for GPRS if requested by the user.",
     "Registered, home network.",
     "Not registered, but MT is currently trying to attach or searching an operator to register to. The GPRS service is enabled, but an allowable PLMN is currently not available. The UE will start a GPRS attach as soon as an allowable PLMN is available.",
-    "Registration denied, The GPRS service is disabled, the UE is not allowed to attach for GPRS if it is requested by the user.",
+    "Registration denied, the GPRS service is disabled, the UE is not allowed to attach for GPRS if it is requested by the user.",
     "Unknown.",
     "Registered, roaming.",
 };
@@ -48,31 +46,28 @@ enum {
     MODEM_CATM_NBIOT,
 };
 
-const int AUTOMATIC_NETWORK = 2;
 
-#define randMax 35
-#define randMin 18
+//!! Set the APN manually. Some operators need to set APN first when registering the network.
+//!! Set the APN manually. Some operators need to set APN first when registering the network.
+//!! Set the APN manually. Some operators need to set APN first when registering the network.
+// Using 7080G with Hologram.io , https://github.com/Xinyuan-LilyGO/LilyGo-T-SIM7080G/issues/19
+// const char *apn = "hologram";
 
-// Your GPRS credentials, if any
-const char apn[] = "internet.itelcel.com";//"emnify";
-const char gprsUser[] = "webgprs";//""; // User
-const char gprsPass[] = "webgprs2002";//""; // Password
+const char *apn = "internet.itelcel.com";
+const char gprsUser[] = "webgprs";
+const char gprsPass[] = "webgprs2002";
 
-// cayenne server address and port
+
+// mosquito address and port
 const char server[]   = "io.adafruit.com";
 const int  port       = 8883;
 char buffer[1024] = {0};
 
 char username[] = "ie714410";
-char password[] = "aio_ggfu01AL4Tnofz7I1Pz7Gv0xFLLy";//"aio_XWlF91lrgcTDWWQWodZqT2hbrfst";
-char clientID[] = "ESP32";
-char topic_alarm[] = "set-alarm";
-char topic_soil_humidity[] = "soil-humidity";
+char password[] = "aio_WSlF17KC643tlUBoVGUKI8lrPeyM";
+char clientID[] = "drake";
 
-int data_channel = 0;
 
-int calculated_moisture, sensed_moisture;
-int moisture_sensor_pin = 1;
 
 bool isConnect()
 {
@@ -84,64 +79,13 @@ bool isConnect()
     return false;
 }
 
-void connect_mqtt()
-{
-    snprintf(buffer, 1024, "+SMCONF=\"URL\",\"%s\",%d", server, port);
-    modem.sendAT(buffer);
-    if (modem.waitResponse() != 1) {
-        return;
-    }
-    snprintf(buffer, 1024, "+SMCONF=\"USERNAME\",\"%s\"", username);
-    modem.sendAT(buffer);
-    if (modem.waitResponse() != 1) {
-        return;
-    }
 
-    snprintf(buffer, 1024, "+SMCONF=\"PASSWORD\",\"%s\"", password);
-    modem.sendAT(buffer);
-    if (modem.waitResponse() != 1) {
-        return;
-    }
-
-    snprintf(buffer, 1024, "+SMCONF=\"CLIENTID\",\"%s\"", clientID);
-    modem.sendAT(buffer);
-    if (modem.waitResponse() != 1) {
-        return;
-    }
-
-    int8_t ret;
-    do {
-
-        modem.sendAT("+SMCONN");
-        ret = modem.waitResponse(30000);
-        if (ret != 1) {
-            Serial.println("Connect failed, retry connect ..."); delay(1000);
-        }
-
-    } while (ret != 1);
-
-    Serial.println("MQTT Client connected!");
-}
-void printResponse() {
-    while (SerialAT.available()) {
-      Serial.write(SerialAT.read());
-    }
-    Serial.println();
-  }
-  
-  void sendATCommand(const char* cmd, const char* expected = "OK", uint32_t timeout = 1000) {
-    Serial.print("AT Command: ");
-    Serial.println(cmd);
-    SerialAT.println(cmd);
-    delay(100);
-    printResponse();
-  }
 void setup()
 {
 
     Serial.begin(115200);
 
-    //Start while waiting for Serial monitoring
+    // Start while waiting for Serial monitoring
     while (!Serial);
 
     delay(3000);
@@ -158,13 +102,21 @@ void setup()
             delay(5000);
         }
     }
-    //Set the working voltage of the modem, please do not modify the parameters
-    PMU.setDC3Voltage(3000);    //SIM7080 Modem main power channel 2700~ 3400V
+
+    // If it is a power cycle, turn off the modem power. Then restart it
+    if (esp_sleep_get_wakeup_cause() == ESP_SLEEP_WAKEUP_UNDEFINED ) {
+        PMU.disableDC3();
+        // Wait a minute
+        delay(200);
+    }
+
+    // Set the working voltage of the modem, please do not modify the parameters
+    PMU.setDC3Voltage(3000);    // SIM7080 Modem main power channel 2700~ 3400V
     PMU.enableDC3();
 
-    //Modem GPS Power channel
+    // Modem GPS Power channel
     PMU.setBLDO2Voltage(3300);
-    PMU.enableBLDO2();      //The antenna power must be turned on to use the GPS function
+    PMU.enableBLDO2();      // The antenna power must be turned on to use the GPS function
 
     // TS Pin detection must be disable, otherwise it cannot be charged
     PMU.disableTSPinMeasure();
@@ -208,14 +160,19 @@ void setup()
         return ;
     }
 
+    // Disable RF
+    modem.sendAT("+CFUN=0");
+    if (modem.waitResponse(20000UL) != 1) {
+        Serial.println("Disable RF Failed!");
+    }
 
     /*********************************
      * step 4 : Set the network mode to NB-IOT
     ***********************************/
 
-    modem.setNetworkMode(AUTOMATIC_NETWORK);    //automatic detection AUTOMATIC_NETWORK=2
+    modem.setNetworkMode(2);    // use automatic
 
-    modem.setPreferredMode(MODEM_CATM);       // NB_IOT(2) as preferred mode
+    modem.setPreferredMode(MODEM_NB_IOT);
 
     uint8_t pre = modem.getPreferredMode();
 
@@ -224,31 +181,33 @@ void setup()
     Serial.printf("getNetworkMode:%u getPreferredMode:%u\n", mode, pre);
 
 
-    /*********************************
-    * step 5 : Wait for the network registration to succeed
-    ***********************************/
-   //check with and without this
-   /* 
-   modem.sendAT("+CNCFG=0,1,\"emnify\"");
+    // Set the APN manually. Some operators need to set APN first when registering the network.
+    modem.sendAT("+CGDCONT=1,\"IP\",\"", apn, "\"");
     if (modem.waitResponse() != 1) {
         Serial.println("Set operators apn Failed!");
         return;
     }
-    modem.sendAT("+CNACT=0,1");
+
+    //!! Set the APN manually. Some operators need to set APN first when registering the network.
+    modem.sendAT("+CNCFG=0,1,\"", apn, "\"");
     if (modem.waitResponse() != 1) {
-        Serial.println("Set operators cnact Failed!");
+        Serial.println("Config apn Failed!");
         return;
     }
-    modem.sendAT("+CAOPEN=0,0,\"TCP\",\"10.189.169.49\",443");
-    if (modem.waitResponse() != 1) {
-        Serial.println("Set operators caopen Failed!");
-        return;
-    }*/
+
+    // Enable RF
+    modem.sendAT("+CFUN=1");
+    if (modem.waitResponse(20000UL) != 1) {
+        Serial.println("Enable RF Failed!");
+    }
+
+    /*********************************
+    * step 5 : Wait for the network registration to succeed
+    ***********************************/
     SIM70xxRegStatus s;
     do {
         s = modem.getRegistrationStatus();
         if (s != REG_OK_HOME && s != REG_OK_ROAMING) {
-            Serial.printf("Response: %u", s);
             Serial.print(".");
             delay(1000);
         }
@@ -259,30 +218,23 @@ void setup()
     Serial.print("Network register info:");
     Serial.println(register_info[s]);
 
-    // Activate network bearer, APN can not be configured by default,
-    // if the SIM card is locked, please configure the correct APN and user password, use the gprsConnect() method
 
     bool res = modem.isGprsConnected();
     if (!res) {
+        // Activate network bearer, APN can not be configured by default,
+        // if the SIM card is locked, please configure the correct APN and user password, use the gprsConnect() method
         modem.sendAT("+CNACT=0,1");
-        //if (modem.waitResponse() != 1) {
-        //    Serial.println("Activate network bearer Failed!");
-         //   return;
-        //}
-         if (!modem.gprsConnect(apn, gprsUser, gprsPass)) {
-             return ;
-         }
+        if (modem.waitResponse() != 1) {
+            Serial.println("Activate network bearer Failed!");
+            return;
+        }
+        // if (!modem.gprsConnect(apn, gprsUser, gprsPass)) {
+        //     return ;
+        // }
     }
 
     Serial.print("GPRS status:");
     Serial.println(res ? "connected" : "not connected");
-
-
-    // Before connecting, you need to confirm that the time has been synchronized.
-    modem.sendAT("+CCLK?");
-    modem.waitResponse(30000);
-
-
 
     /*********************************
     * step 6 : setup MQTT Client
@@ -293,34 +245,67 @@ void setup()
     modem.waitResponse();
 
 
-    connect_mqtt();
+    snprintf(buffer, 1024, "+SMCONF=\"URL\",\"%s\",%d", server, port);
+    modem.sendAT(buffer);
+    if (modem.waitResponse() != 1) {
+        return;
+    }
+    snprintf(buffer, 1024, "+SMCONF=\"USERNAME\",\"%s\"", username);
+    modem.sendAT(buffer);
+    if (modem.waitResponse() != 1) {
+        return;
+    }
+
+    snprintf(buffer, 1024, "+SMCONF=\"PASSWORD\",\"%s\"", password);
+    modem.sendAT(buffer);
+    if (modem.waitResponse() != 1) {
+        return;
+    }
+
+    snprintf(buffer, 1024, "+SMCONF=\"CLIENTID\",\"%s\"", clientID);
+    modem.sendAT(buffer);
+    if (modem.waitResponse() != 1) {
+        return;
+    }
+
+    int8_t ret;
+    do {
+
+        modem.sendAT("+SMCONN");
+        ret = modem.waitResponse(30000);
+        if (ret != 1) {
+            Serial.println("Connect failed, retry connect ..."); delay(1000);
+        }
+
+    } while (ret != 1);
 
 
-    sendATCommand("+SMSUB?");
-// Subscribe to MQTT topic
-    snprintf(buffer, 1024, "+SMSUB=\"%s/feeds/%s\",1", username, topic_alarm);
-    sendATCommand(buffer);
-    //Serial.println(modem.waitResponse(10000));
-    //if (modem.waitResponse() != 1) {
-    //    return;
-    //}
-    sendATCommand("+SMSUB?");
+    Serial.println("MQTT Client connected!");
+
+
+    /*********************************
+    * step 7 : Subscribe topic
+    ***********************************AT+SMSUB=”exampleTopic”,1 */
+    snprintf(buffer, 1024, "+SMSUB=\"ie714410/feeds/set-alarm\",1");
+    modem.sendAT(buffer);
+    if (modem.waitResponse() != 1) {
+        return;
+    }
+
     Serial.print("MQTT Subscribe topic : ");
-    Serial.println(buffer);    
-    // random seed data
-    //randomSeed(esp_random());
+    Serial.println(buffer);
+
 }
+
 
 void loop()
 {
     if (!isConnect()) {
         Serial.println("MQTT Client disconnect!"); delay(1000);
-        Serial.println("Connecting...");
-        connect_mqtt();
+        return ;
     }
-    //Check if subscription has messages
-    Serial.println("Checking for messages:");
-    delay(2000);
+
+    // Waiting for subscribed messages
     if (modem.waitResponse("+SMSUB: ") == 1) {
         String result =  modem.stream.readStringUntil('\r');
         Serial.print("Recive payload:");
@@ -335,9 +320,9 @@ void loop()
         result = result.substring(index + 1);
         result.replace("\"", "");
 
-        //Get command value
+        // Get command value
         char value = result[result.length() - 1];
-        //Get Sep
+        // Get Sep
         result = result.substring(0, result.length() - 2);
 
         String payload = "ok,";
@@ -345,40 +330,10 @@ void loop()
 
         if (value == '1') {
             PMU.setChargingLedMode(XPOWERS_CHG_LED_ON);
-            
-          //  tone(BUZZZER_PIN, NOTE_C4, 1000);
-          //  delay(1000);
-          //  noTone(BUZZZER_PIN);
         } else {
             PMU.setChargingLedMode(XPOWERS_CHG_LED_OFF);
         }
-    }else
-    {
-        Serial.println("subscription has no messages");
-    }
 
-    sensed_moisture = analogRead(moisture_sensor_pin);
-    calculated_moisture = (100 - ((sensed_moisture/4095)*100));
-    String payload = String(calculated_moisture)+ "\r\n";
-    snprintf(buffer, 1024, "+SMPUB=\"%s/feeds/%s\",%d,1,1", username, topic_soil_humidity, payload.length());
-    modem.sendAT(buffer);
-    if (modem.waitResponse(">") == 1) {
-        modem.stream.write(payload.c_str(), payload.length());
-        Serial.print("Try publish payload: ");
-        Serial.println(payload);
-
-        if (modem.waitResponse(3000)) {
-            Serial.println("Send Packet success!");
-        } else {
-            Serial.println("Send Packet failed!");
-        }
-    }
-    //Serial.println(buffer);
-    //Serial.println(payload);
-    Serial.println("Moisture level: "+String(calculated_moisture)+"%");
-
-    
-/*
         // v1/username/things/clientID/response
         snprintf(buffer, 1024, "+SMPUB=\"v1/%s/things/%s/response\",%d,1,1", username, clientID, payload.length());
         modem.sendAT(buffer);
@@ -395,8 +350,4 @@ void loop()
         }
 
     }
-    */
-    
-    //Sleep and wakeup block
-    delay(6000);
 }
